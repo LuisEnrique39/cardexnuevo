db88aaa073f1d4ea15b5633dad77809112db7e5d
LuisEnrique39 <luiscruznoguez39425@gmail.com>
2023-11-26 20:38:13 -0600

    second commit

--- cardex2/cardex/apps/cliente/config.py
+++
@@ -1,7 +0,0 @@
-
-from django.apps import AppConfig
-
-
-class ClientConfig(AppConfig):
-    name = 'apps.cliente'
-    label = 'apps_cliente'
--- cardex2/cardex/apps/home/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import UserInfo
-
-class PerfilForm(forms.ModelForm):
-    class Meta:
-        model = UserInfo
-        fields = ['foto_de_perfil']
-
